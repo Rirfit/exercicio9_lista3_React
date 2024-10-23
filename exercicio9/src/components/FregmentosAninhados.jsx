1601134bc97e2d0432959c896e4299d8b8efa8f4
Rirfit <lilrirfit@gmail.com>
2024-10-23 17:59:35 -0300

    first commit

---
+++ exercicio9/src/components/FregmentosAninhados.jsx
@@ -0,0 +1,13 @@
+function FragmentosAninhados(){
+    return(
+        <>
+        <h1>Titulo dos fragmentos</h1>
+        <ul>
+            <li>Lista 1</li>
+            <li>lista 2</li>
+            <li>lista 3</li>
+        </ul>
+        <p>parágrafo dos Fragmentos Aninhados</p>
+        </>
+    )
+}export default FragmentosAninhados
